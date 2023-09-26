79fc6a4aee76142d36f6af0b52f54f370d3df0ed
echiaradia <37142565+echiaradia@users.noreply.github.com>
2023-09-26 22:06:02 +0200

    add version to exe
    get latest commit code and add it to the name of the executable

--- compile_CropCoef_v5.bat
+++ compile_CropCoef_v5.bat
@@ -1,5 +1,11 @@
 SET PATH=%PATH%;C:\MinGW\bin
 
+:: set the version code base on the latest commit
+:: dirty means with local edits
+FOR /F "delims=" %%i IN ('git describe --abbrev^=7 --dirty --always') DO set version=%%i
+
+echo latest git commit=%version%
+
 set year=%date:~-4%
 echo year=%year%
 set month=%date:~3,2%
@@ -22,11 +28,11 @@ gfortran -fbounds-check -march=prescott -ffast-math -funroll-loops -O3 -c mod_io
 gfortran -fbounds-check -march=prescott -ffast-math -funroll-loops -O3 -c mod_cropcoef_v4.f90
 gfortran -fbounds-check -march=prescott -ffast-math -funroll-loops -O3 -c main.f90
 
-gfortran -o ./example/cropcoef_%datetimef%.exe *.o -static
+gfortran -o ./example/cropcoef_%version%_%datetimef%.exe *.o -static
 
 
 erase *.o *.mod
 
 cd ./example/
 
-cropcoef_%datetimef%.exe -f cropcoef.txt -v
+cropcoef_%version%_%datetimef%.exe -f cropcoef.txt -v
